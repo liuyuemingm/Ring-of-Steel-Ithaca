6636581455e6b331958d8303fe054870609a64d5
Yueming <yl3436@cornell.edu>
2021-08-13 00:13:35 -0400

    Animation using useEffect()

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import react from "react"
 import Footer from "./components/Footer.jsx"
 import Header from "./components/Header.jsx"
 import Drills from "./components/Drills.jsx"
+import PlayButton from "./components/PlayButton.jsx"
 
 function App() {
   return (
---
+++ src/components/PlayAni.jsx
@@ -0,0 +1,19 @@
+import { actions } from './Parry_1';
+import { useEffect } from 'react'
+
+
+function PlayAni() {
+
+  try {
+    useEffect(() => {
+      actions.guard_3_hand.play();
+      // actions.guard_3_hand.repetitions = 3;
+    })
+  }
+  catch (err) {
+    alert(err.message);
+  }
+  return null
+}
+
+export default PlayAni
