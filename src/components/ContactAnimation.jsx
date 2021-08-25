27f89e0513e2438a5a71602c10ffa633289779fd
Yueming <yl3436@cornell.edu>
2021-08-24 22:52:09 -0400

    Contact page finished

--- src/components/ContactAnimation.jsx
+++ src/components/ContactAnimation.jsx
@@ -1,8 +1,6 @@
 
 import React, { useRef } from 'react'
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import WeaponCard from './WeaponCard'
-import { Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 const appHeight = '64';
 
@@ -12,7 +10,10 @@ const useStyles = makeStyles((theme) => ({
     height: `calc(100vh - ${appHeight}px)`,
     textAlign: 'center',
     position: 'relative',
-    background: '#222222'
+    background: '#222222',
+    fontSize: '16px',
+    color: 'rgba(255, 190, 0, 1)',
+
   },
 
   body: {
@@ -44,12 +45,7 @@ const useStyles = makeStyles((theme) => ({
     transform: 'translate(-50%, -50%) rotate(-90deg)',
     width: '34%',
     transition: '0.3s',
-    '&:hover': {
-      top: '30%',
-      left: '-35%',
-      width: '45%',
-      transform: 'rotate(-30deg)'
-    },
+
   },
 
   president: {
@@ -59,12 +55,6 @@ const useStyles = makeStyles((theme) => ({
     transform: 'translate(-50%, -50%) ',
     width: '29%',
     transition: '0.3s',
-    '&:hover': {
-      top: '-12%',
-      left: '-15%',
-      width: '36%',
-      transform: 'rotate(45deg)',
-    },
   },
 
   campusgroups: {
@@ -74,10 +64,8 @@ const useStyles = makeStyles((theme) => ({
     transform: 'translate(-50%, -50%) ',
     width: '19%',
     transition: '0.3s',
-    '&:hover': {
-      top: '-15%',
-      left: '50%',
-      width: '30%',
+    '& $name': {
+      display: 'hidden'
     },
   },
 
@@ -86,14 +74,8 @@ const useStyles = makeStyles((theme) => ({
     top: '7%',
     left: '91%',
     transform: 'translate(-50%, -50%) ',
-    width: '35%',
+    width: '52.5%',
     transition: '0.3s',
-    '&:hover': {
-      top: '-8%',
-      left: '85%',
-      width: '40%',
-      transform: 'rotate(-135deg)'
-    },
   },
 
   armorer: {
@@ -103,12 +85,6 @@ const useStyles = makeStyles((theme) => ({
     transform: 'translate(-50%, -50%) rotate(90deg)',
     width: '32%',
     transition: '0.3s',
-    '&:hover': {
-      top: '24%',
-      left: '88%',
-      width: '43%',
-      transform: 'rotate(0deg)'
-    },
   },
 
   eyeball: {
@@ -116,9 +92,109 @@ const useStyles = makeStyles((theme) => ({
     top: '51%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: '14%',
+    width: '12%',
     transition: '0.2s',
-    transformOrigin: '50% 50%'
+    transformOrigin: '50% 51%'
+  },
+
+  name: {
+    position: 'absolute',
+    width: 'fit-content',
+    textShadow: '0 0 5px orange'
+  },
+
+  treasurerContainer: {
+    '& $name': {
+      opacity: '0'
+    },
+    '&:hover': {
+      '& $treasurer': {
+        top: '30%',
+        left: '-35%',
+        width: '45%',
+        transform: 'rotate(-30deg)'
+      },
+      '& $name': {
+        transition: '0.3s',
+        opacity: '1'
+      },
+    },
+  },
+
+  presidentContainer: {
+    '& $name': {
+      opacity: '0'
+    },
+    '&:hover': {
+      '& $president': {
+        top: '-12%',
+        left: '-15%',
+        width: '36%',
+        transform: 'rotate(45deg)',
+      },
+      '& $name': {
+        transition: '0.3s',
+        opacity: '1'
+      },
+    },
+  },
+
+  campusgroupsContainer: {
+    '& $name': {
+      opacity: '0'
+    },
+    '&:hover': {
+      '& $campusgroups': {
+        top: '-15%',
+        left: '50%',
+        width: '30%',
+      },
+      '& $name': {
+        transition: '0.3s',
+        opacity: '1'
+      },
+    },
+  },
+
+  vicepresidentContainer: {
+    '& $name': {
+      opacity: '0'
+    },
+    '&:hover': {
+      '& $vicepresident': {
+        top: '-18%',
+        left: '75%',
+        width: '60%',
+        transform: 'rotate(-135deg)',
+      },
+      '& $name': {
+        transition: '0.3s',
+        opacity: '1'
+      },
+    },
+  },
+
+  armorerContainer: {
+    '& $name': {
+      opacity: '0'
+    },
+    '&:hover': {
+      '& $armorer': {
+        top: '24%',
+        left: '88%',
+        width: '43%',
+        transform: 'rotate(0deg)'
+      },
+      '& $name': {
+        transition: '0.3s',
+        opacity: '1'
+      },
+    },
+  },
+
+  link: {
+    textDecoration: 'none',
+    color: 'white'
   }
 
 }));
@@ -142,8 +218,8 @@ function ContactAnimation() {
 
     var rotateY = -(clientY - 64) / (window.innerHeight - 64) + 0.5;
     var deg = Math.sqrt((Math.abs(rotateX) + Math.abs(rotateY))) * 45;
-    var translateX = -50 + rotateX * 25 + '%';
-    var translateY = -50 - rotateY * 25 + '%';
+    var translateX = -50 + rotateX * 45 + '%';
+    var translateY = -51 - rotateY * 45 + '%';
 
 
     eyeball.current.style.transform =
@@ -162,14 +238,62 @@ function ContactAnimation() {
         <img src='/images/contact-eyeframe.png' className={classes.eyeframe} />
         <img src='/images/contact-pupil.png' className={classes.eyeball} ref={eyeball} />
 
-        <img src='/images/contact-treasurer.png' className={classes.treasurer} />
-        <img src='/images/contact-president.png' className={classes.president} />
-        <img src='/images/contact-campusgroups.png' className={classes.campusgroups} />
-        <img src='/images/contact-vicepresident.png' className={classes.vicepresident} />
-        <img src='/images/contact-armorer.png' className={classes.armorer} />
+        <div className={classes.treasurerContainer}>
+          <img src='/images/contact-treasurer.png' className={classes.treasurer} />
+          <p className={classes.name}
+            style={{ left: '-38%', top: '45%' }}>
+            Treasurer
+            <br /> Austin C.
+          </p>
+        </div>
+
+        <div className={classes.presidentContainer}>
+          <img src='/images/contact-president.png' className={classes.president} />
+          <p className={classes.name}
+            style={{ left: '-28%', top: '14%' }}>
+            President
+            <br /> Kaitlin Smith
+          </p>
+        </div>
+
+
+        <div className={classes.campusgroupsContainer}>
+          <img src='/images/contact-campusgroups.png' className={classes.campusgroups} />
+          <a className={classes.link} href='https://cornell.campusgroups.com/ROSI/club_signup' target="_blank" rel="noopener noreferrer">
+            <p className={classes.name}
+              style={{ left: '9%', top: '-28%' }}>
+              Cornell  <img src='/images/link.png' style={{ width: '12px' }} />
+              <br /> Campus Groups
+            </p>
+          </a>
+        </div>
+
+
+
+        <div className={classes.vicepresidentContainer}>
+          <img src='/images/contact-vicepresident.png' className={classes.vicepresident} />
+          <a className={classes.link} href='https://www.linkedin.com/in/yueming-liu-9aba84196/' target="_blank" rel="noopener noreferrer">
+            <p className={classes.name}
+              style={{ left: '90%', top: '-10%' }}>
+              Vice President
+              <br /> U'Ming Liu <img src='/images/link.png' style={{ width: '12px' }} />
+            </p>
+          </a>
+        </div>
+
+
+        <div className={classes.armorerContainer}>
+          <img src='/images/contact-armorer.png' className={classes.armorer} />
+          <p className={classes.name}
+            style={{ left: '121%', top: '30%' }}>
+            Armorer
+            <br /> Amy W.
+          </p>
+        </div>
 
       </div>
-    </div>
+    </div >
+
   )
 }
 
