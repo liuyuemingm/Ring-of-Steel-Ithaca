9d61828092d725e0f8e154fc08dd041b95b9bb01
Yueming <yl3436@cornell.edu>
2021-09-06 10:24:30 -0400

    GitHub page deploy

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
 
   return (
 
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <ThemeProvider theme={theme}>
           <Switch>
